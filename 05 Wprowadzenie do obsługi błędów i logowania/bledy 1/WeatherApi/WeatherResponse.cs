bfcd55c72971ffd85e5babf6bb11ee6ea5787c3b
Jakubburczynski <viceroy1888@gmail.com>
2026-08-23 21:53:38 +0200

    Add files via upload

---
+++ 05 Wprowadzenie do obsługi błędów i logowania/bledy 1/WeatherApi/WeatherResponse.cs
@@ -0,0 +1,27 @@
+using WeatherApi.Classes;
+
+public class WeatherResponse
+{
+    public MainData Main { get; set; } = new();
+    public WindData Wind { get; set; } = new();
+    public List<WeatherData> Weather { get; set; } = new();
+    public string Name { get; set; } = "";
+}
+
+public class MainData
+{
+    public double Temp { get; set; }
+    public double Feels_Like { get; set; }
+    public int Humidity { get; set; }
+}
+
+public class WindData
+{
+    public double Speed { get; set; }
+}
+
+public class WeatherData
+{
+    public string Main { get; set; } = "";
+    public string Description { get; set; } = "";
+}
---
+++ 05 Wprowadzenie do obsługi błędów i logowania/bledy 2/WeatherApi/controllers/TestErrorsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestErrorsController : ControllerBase
+{
+    private readonly ILogger<TestErrorsController> _logger;
+
+    public TestErrorsController(ILogger<TestErrorsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        _logger.LogInformation("Pobrano dane z TestErrorsController");
+
+        return Ok("Test logowania");
+    }
+}
